323b210f4481f14f4f1a2979041c7aea91dadc3a
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-06 14:19:59 +0100

    change pin address

--- entry.sh
+++ entry.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 import time
-import adafruit_dht
 import board 
+import digitalio
+import adafruit_dht
+
+pin = digitalio.DigitalInOut(board.D4)
+print("Digital IO ok!")
 
 dht_device = adafruit_dht.DHT11(board.D4)
 
