c090fbe4e8d099f44718083832c28a970ca5d1ef
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-09 19:19:01 +0100

    create class to read one wire sensor and provide dict output

--- read_sensor/w1.py
+++ read_sensor/w1.py
@@ -6,10 +6,26 @@ import time
 import datetime
 import os
 
-from w1thermsensor import W1ThermSensor
+#from w1thermsensor import W1ThermSensor
+#sensor = W1ThermSensor()
 
-sensor = W1ThermSensor()
-while True:
-    current_temperature=sensor.get_temperature()
-    print(datetime.datetime.now(), "\t",current_temperature)
-    time.sleep(5)
+class TempSens:
+    def __init__(self, sid=None):        
+        from w1thermsensor import W1ThermSensor
+        self.sensor = W1ThermSensor(sensor_id=sid)
+            
+    def get_readings(self, sensor):
+        current_temperature = self.sensor.get_temperature()
+        return [
+            {
+                'measurement': 'home-data',
+                'fields': {
+                    'temperature': float(current_temperature)
+                }
+            }
+        ]    
+
+#while True:
+#    current_temperature=sensor.get_temperature()
+#    print(datetime.datetime.now(), " T=",current_temperature," C")
+#    time.sleep(5)
