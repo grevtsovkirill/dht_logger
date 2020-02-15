a7e7b1d6933a2b694826323e4c912f8da1a21805
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-15 14:22:27 +0100

    transmit data to http server

--- read_sensor/driver.py
+++ read_sensor/driver.py
@@ -4,6 +4,8 @@ import os
 import json
 
 from w1 import TempSens
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
 #from lucky_n import LUCKY
 
 class read_data():
@@ -14,7 +16,27 @@ class read_data():
         return self.sensor.get_readings(self.sensor)
 
     
+class homeDataHTTP(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        measurements = rd.sample()
+        self.wfile.write(json.dumps(measurements[0]['fields']).encode('UTF-8'))
+
+    def do_HEAD(self):
+        self._set_headers()
+
+rd = read_data()
+        
 while True:
-    rd = read_data()
-    measurements = rd.sample()
-    print(measurements[0]['fields'])
+    #measurements = rd.sample()
+    #print(measurements[0]['fields'])
+    server_address = ('', 80)
+    httpd = HTTPServer(server_address, homeDataHTTP)
+    print('Sensor HTTP server running')
+    httpd.serve_forever()
+
