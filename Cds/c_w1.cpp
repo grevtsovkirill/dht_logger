a7969a224ac2b9c23a43697745856d1a476365dd
Kirill Grevtsov <grevtsovkirill@gmail.com>
2020-03-15 15:44:39 +0000

    measurements in the loop, with 5 sec delay

--- Cds/c_w1.cpp
+++ Cds/c_w1.cpp
@@ -8,19 +8,22 @@ To run; ./run_w1
 #include <string>
 #include "DS18B20.h"
 #include <ctime>
+#include <thread>         
+#include <chrono>         
 using namespace std;
 
 int main() {
     double tempNow;
     char w1_address[16]="28-00000484b7e5";
 
+    while(1){
+      DS18B20 w1Device1 (w1_address);
+      tempNow = w1Device1.getTemp();
+      
+      time_t now = time(0);
+      char* dt = ctime(&now);
+      cout <<dt<<   " T=" << tempNow << " C" <<endl;
+      this_thread::sleep_for(std::chrono::seconds(5));
+    }
 
-    DS18B20 w1Device1 (w1_address);
-    tempNow = w1Device1.getTemp();
-
-    time_t now = time(0);
-    char* dt = ctime(&now);
-    cout <<dt<<   " T=" << tempNow << " C" <<endl;
-
-    return 0;
 }
