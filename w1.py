3e2a07c1b5f29d1d6412ff0af9820ec9e061b775
grevtsovkirill <grevtsovkirill@gmail.com>
2020-03-16 22:21:20 +0000

    make printout in InfluxDB format

--- w1.py
+++ w1.py
@@ -12,10 +12,13 @@ def get_line_protocol(sensor, reading, value):
 	timestamp = str(int(datetime.datetime.now().timestamp() * 1000))
 	return line.format(sensor, reading, value, timestamp)
     
-
 sensor = W1ThermSensor()
 while True:
     current_temperature=sensor.get_temperature()
-    print(datetime.datetime.now(), "\t",current_temperature)
-    time.sleep(60)
+    #print(datetime.datetime.now(), "\t",current_temperature)
+    sens='DS18B20'
+    reading='T'
+    value=current_temperature
+    print(get_line_protocol(sens, reading, value))
+    time.sleep(5)
 
