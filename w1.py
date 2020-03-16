8b3cc5028bc985576d3c36d2e037fde96ab2420d
grevtsovkirill <grevtsovkirill@gmail.com>
2020-03-16 22:16:47 +0000

    function to read out sensor data in InfluxDB line protocol

--- w1.py
+++ w1.py
@@ -7,8 +7,15 @@ import datetime
 
 from w1thermsensor import W1ThermSensor
 
+def get_line_protocol(sensor, reading, value):
+	line = "{} {}={} {}"
+	timestamp = str(int(datetime.datetime.now().timestamp() * 1000))
+	return line.format(sensor, reading, value, timestamp)
+    
+
 sensor = W1ThermSensor()
 while True:
     current_temperature=sensor.get_temperature()
     print(datetime.datetime.now(), "\t",current_temperature)
-    time.sleep(5)
+    time.sleep(60)
+
