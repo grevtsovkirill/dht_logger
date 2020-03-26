72a91727ef90be6910cdc860493853b1ee70f536
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-03-26 23:55:53 +0100

    connect Bluethooth HC-05, transmit measurements, resolves #12

--- arduino_data/arduino_data.ino
+++ arduino_data/arduino_data.ino
@@ -2,11 +2,15 @@
 #include <dht.h>
 
 dht DHT;
-
 #define DHT11_PIN 22
 
+#include <SoftwareSerial.h>
+SoftwareSerial EEBlue(0, 1); // RX | TX
+ 
 void setup(){
   Serial.begin(9600);
+  EEBlue.begin(9600);  //Default Baud for comm, it may be different for your Module. 
+  Serial.println("The bluetooth gates are open.\n Connect to HC-05 from any other bluetooth device with 1234 as pairing key!.");
 }
 
 void loop()
