ce77696d476f718a3a78be7df96456276f05d17d
grevtsovkirill <grevtsovkirill@gmail.com>
2020-03-27 15:12:44 +0000

    convert outT to functions

--- outside_T.py
+++ outside_T.py
@@ -4,15 +4,28 @@ from urllib.request import urlopen
 import json
 from user_info import *
 
-input_type = "latlon"
-if input_type is "id":
-    url="http://api.openweathermap.org/data/2.5/weather?id="+str(city_id)+"&APPID="+apikey
-elif input_type is "latlon":
-    url="http://api.openweathermap.org/data/2.5/weather?lat="+str(lat)+"&lon="+str(lon)+"&APPID="+apikey+"&units=metric"
-print(url)
-meteo=urlopen(url).read()
-meteo = meteo.decode('utf-8')
-weather = json.loads(meteo)
+def request_data():
+    input_type = "latlon"
+    if input_type is "id":
+        url="http://api.openweathermap.org/data/2.5/weather?id="+str(city_id)+"&APPID="+apikey
+    elif input_type is "latlon":
+        url="http://api.openweathermap.org/data/2.5/weather?lat="+str(lat)+"&lon="+str(lon)+"&APPID="+apikey+"&units=metric"
+        print(url)
+        meteo=urlopen(url).read()
+        meteo = meteo.decode('utf-8')
+        weather = json.loads(meteo)
+        return weather
+        
+def get_out_reading(read_type):
+    print(read_type)
+    
 
-for key in weather['main']:
-    print (key," = ",weather['main'][key] )
+def main():
+    weather = request_data()
+    for key in weather['main']:
+        print (key," = ",weather['main'][key] )
+
+    read_types = ['temp','pressure','humidity']
+    
+if __name__== "__main__":
+  main()    
