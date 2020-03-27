8418ec7635fa8780e35eceee222f972ab9883741
grevtsovkirill <grevtsovkirill@gmail.com>
2020-03-27 14:09:21 +0000

    reorganize personal info - to keep example of how setup should look like, but real info will be out of versioning

---
+++ user_info.py
@@ -0,0 +1,5 @@
+
+apikey = "yourAPIkey" # get Free account at openweathermap.org/
+city_id = 1234567 # search for your city ID
+lat = 99.99999 # specify latitude 
+lon = 0.00000  # specify longitude
