a3fcc753132dd50002cf7861f257feadeabfd0f4
grevtsovkirill <grevtsovkirill@gmail.com>
2020-05-24 16:15:25 +0100

    read Serial Port (BT from Arduino measurements), convert it to line protocol output. resolves #25

--- dht_logger/btsr.py
+++ dht_logger/btsr.py
@@ -1,7 +1,10 @@
 import time
 import serial
           
-      
+sens='DTH11'
+measurement_dict = {'Temperature':'T_liv','Humidity':'H_liv'}
+read_types = ['Temperature','Humidity']
+
 ser = serial.Serial(
     
     port='/dev/rfcomm0',
@@ -11,9 +14,31 @@ ser = serial.Serial(
     bytesize=serial.EIGHTBITS,
     timeout=1
 )
-counter=0
 
 
-while 1:
-    x=ser.readline()
-    print(x)
+#     #b'Humidity = 23.00\r\n'
+#     #b'Temperature = 16.00\r\n'
+
+def get_sp_data(read_type):
+    #x=''
+    while 1:
+        x=str(ser.readline())
+        if read_type in x:
+            y = x.find(read_type)+len(read_type)+3
+            z=x[y:y+5]
+            #print(z)
+            return z
+            break
+
+def get_bt_reading(read_type):
+    return sens,measurement_dict[read_type],get_sp_data(read_type)
+
+def main():
+    #meas = get_sp_data()
+    for i in read_types:
+        print(i)
+        #get_sp_data(i)
+        print(get_bt_reading(i))
+
+if __name__== "__main__":
+  main()    
